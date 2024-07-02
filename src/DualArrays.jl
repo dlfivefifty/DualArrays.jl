c2477962e87167f261d4936139520fb71d972b2a
Sheehan Olver <solver@mac.com>
2024-07-02 16:04:58 +0100

    basic interface

--- src/DualArrays.jl
+++ src/DualArrays.jl
@@ -1,6 +1,20 @@
 module DualArrays
-export greet
+export DualVector
 
-greet() = "Bye World!"
+
+"""
+DualVector(value, jacobian)
+
+reprents a vector of duals given by
+    
+    values + jacobian * [ε_1,…,ε_n].
+
+For now the entries just return the values.
+"""
+
+struct DualVector{T} <: AbstractVector{T}
+    value::Vector{T}
+    jacobian::Matrix{T}
+end
 
 end # module DualArrays
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,17 @@
 using DualArrays, Test
 
 @testset "DualArrays" begin
-    @test  greet() == "Bye World!"
+    v = DualVector([1,2], [1 2; 3 4])
+    @test v[1] == 1
+    @test v[2] == 2
+    @test_throws BoundsError v[3]
+    @test v == [1,2]
+
+    w = v + v
+    @test w == [2,4]
+    @test w.jacobian == 2v.jacobian
+
+    @test sin.(v) isa DualVector
+    @test sin.(v) == [sin(1), sin(2)]
+    @test sin.(v).partials == Diagonal(cos.(v.value)) * v.partials
 end
