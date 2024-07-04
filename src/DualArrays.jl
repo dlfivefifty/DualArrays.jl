cdee041635b4f9f15398fd61cccb3dea7c04c423
maximvassiliev <vassiliev.max@gmail.com>
2024-07-04 23:09:27 +0100

    add multiplication and sum

--- src/DualArrays.jl
+++ src/DualArrays.jl
@@ -1,7 +1,7 @@
 module DualArrays
 export DualVector
 using LinearAlgebra, ForwardDiff
-import Base: +, getindex, size, broadcast, axes, broadcasted, show
+import Base: +, getindex, size, broadcast, axes, broadcasted, show, sum
 
 """
 reprents a vector of duals given by
@@ -30,6 +30,17 @@ axes(x::DualVector) = axes(x.value)
 +(x::DualVector,y::DualVector) = DualVector(x.value + y.value, x.jacobian + y.jacobian)
 
 broadcasted(::typeof(sin),x::DualVector) = DualVector(sin.(x.value),Diagonal(cos.(x.value))*x.jacobian)
+
+function broadcasted(::typeof(*),x::DualVector,y::DualVector)
+    newval = x.value .* y.value
+    newjac = x.value .* y.jacobian + y.value .* x.jacobian
+    DualVector(newval,newjac)
+end
+
+function sum(x::DualVector)
+    n = length(x.value)
+    ForwardDiff.Dual(sum(x.value),Tuple(sum(x.jacobian[:,i]) for i=1:n))
+end
 
 show(io::IO,::MIME"text/plain", x::DualVector) = (print(io,x.value); print(io," + "); print(io,x.jacobian);print("𝛜"))
 end
