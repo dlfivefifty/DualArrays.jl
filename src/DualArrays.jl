6578c9479b1bc53659da3d3cc8e32aecc8ca05e1
Maxim Vassiliev <76599693+max-vassili3v@users.noreply.github.com>
2024-07-05 22:50:00 +0100

    UnitRange selection, .* and sum() (#1)
    * getindex with UnitRange, getindex with integer returning a (ForwardDiff) Dual. Utility show() overload
    
    * add multiplication and sum
    
    * update unit tests
    
    * Use banded matrices
    
    * Use custom dual
    
    * Update runtests.jl
    
    * add error handling for dimension mismatches
    
    ---------
    
    Co-authored-by: Sheehan Olver <solver@mac.com>

--- src/DualArrays.jl
+++ src/DualArrays.jl
@@ -1,7 +1,16 @@
 module DualArrays
 export DualVector
-using LinearAlgebra
-import Base: +, getindex, size, broadcast, axes, broadcasted
+using LinearAlgebra, ArrayLayouts, BandedMatrices
+import Base: +, ==, getindex, size, broadcast, axes, broadcasted, show, sum
+
+
+struct Dual{T} <: Real
+    value::T
+    partials::Vector{T}
+end
+
+==(a::Dual, b::Dual) = a.value == b.value && a.partials == b.partials
+
 """
 reprents a vector of duals given by
     
@@ -10,16 +19,51 @@ reprents a vector of duals given by
 For now the entries just return the values.
 """
 
-struct DualVector{T, M <: AbstractMatrix{T}} <: AbstractVector{T}
+struct DualVector{T, M <: AbstractMatrix{T}} <: AbstractVector{Dual{T}}
     value::Vector{T}
     jacobian::M
+    function DualVector(value::Vector{T},jacobian::M) where {T, M <: AbstractMatrix{T}}
+        if(size(jacobian)[1] != length(value))
+            x,y = length(value),size(jacobian)[1]
+            throw(ArgumentError("vector length must match number of rows in jacobian.\n
+            vector length: $x \n
+            no. of jacobian rows: $y"))
+        end
+        new{T,M}(value,jacobian)
+    end
 end
 
-getindex(x::DualVector,y::Int) = x.value[y]
+function DualVector(value::AbstractVector, jacobian::AbstractMatrix)
+    T = promote_type(eltype(value), eltype(jacobian))
+    DualVector(convert(Vector{T}, value), convert(AbstractMatrix{T}, jacobian))
+end
+
+function getindex(x::DualVector, y::Int)
+    Dual(x.value[y], x.jacobian[y,:])
+end
+
+function getindex(x::DualVector, y::UnitRange)
+    newval = x.value[y]
+    newjac = layout_getindex(x.jacobian,y,:)
+    DualVector(newval, newjac)
+end
 size(x::DualVector) = length(x.value)
 axes(x::DualVector) = axes(x.value)
 +(x::DualVector,y::DualVector) = DualVector(x.value + y.value, x.jacobian + y.jacobian)
 
 broadcasted(::typeof(sin),x::DualVector) = DualVector(sin.(x.value),Diagonal(cos.(x.value))*x.jacobian)
+
+function broadcasted(::typeof(*),x::DualVector,y::DualVector)
+    newval = x.value .* y.value
+    newjac = x.value .* y.jacobian + y.value .* x.jacobian
+    DualVector(newval,newjac)
+end
+
+function sum(x::DualVector)
+    n = length(x.value)
+    Dual(sum(x.value), vec(sum(x.jacobian; dims=1)))
+end
+
+show(io::IO,::MIME"text/plain", x::DualVector) = (print(io,x.value); print(io," + "); print(io,x.jacobian);print("𝛜"))
 end
 # module DualArrays
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,37 @@
-using DualArrays, Test, LinearAlgebra
+using DualArrays, Test, LinearAlgebra, ForwardDiff, BandedMatrices
+using DualArrays: Dual
 
 @testset "DualArrays" begin
-    v = DualVector([1,2], [1 2; 3 4])
-    @test v[1] == 1
-    @test v[2] == 2
-    @test_throws BoundsError v[3]
-    @test v == [1, 2]
+    @test_throws ArgumentError DualVector([1,2],I(3))
+    
+    v = DualVector([1,2, 3], [1 2 3; 4 5 6;7 8 9])
+    @test v[1] isa Dual
+    @test v[1] == Dual(1,[1,2,3])
+    @test v[2] == Dual(2,[4,5,6])
+    @test v[3] == Dual(3,[7,8,9])
+    @test_throws BoundsError v[4]
+    @test v == DualVector([1,2, 3], [1 2 3; 4 5 6;7 8 9])
 
     w = v + v
-    @test w == [2,4]
+    @test w == DualVector([2,4,6],[2 4 6;8 10 12;14 16 18])
     @test w.jacobian == 2v.jacobian
 
     @test sin.(v) isa DualVector
-    @test sin.(v) == [sin(1), sin(2)]
+    @test sin.(v).value == [sin(1), sin(2), sin(3)]
     @test sin.(v).jacobian == Diagonal(cos.(v.value)) * v.jacobian
+
+    x,y = v[1:2],v[2:3]
+    @test x == DualVector([1,2],[1 2 3;4 5 6])
+    @test y == DualVector([2,3],[4 5 6;7 8 9])
+    @test x .* y == DualVector([2,6],[6 9 12;26 31 36])
+    
+    @test sum(x .* y) isa Dual
+    @test sum(x .* y) == Dual(8,[32,40,48])
+
+    n = 10
+    v = DualVector(1:n, I(n))
+    @test v[2:end].jacobian isa BandedMatrix
+
+
+    @test sum(v[1:end-1] .* v[2:end]).partials == ForwardDiff.gradient(v -> sum(v[1:end-1] .* v[2:end]), 1:n)
 end
