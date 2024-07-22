652598d5935c9f0223462efd86a8a23a65ff3f40
maximvassiliev <vassiliev.max@gmail.com>
2024-07-22 14:41:37 +0100

    Initial forwarddiff implementation

---
+++ examples/newtonpendulum.jl
@@ -0,0 +1,48 @@
+##
+# Solve pendulum ODE:
+
+# x'' + sin(x) = 0
+
+# via discretisation and Newton's method.
+##
+
+using LinearAlgebra, ForwardDiff, Plots
+
+#Boundary Conditions
+a = 0.1
+b = 0.0
+
+#Time step, Time period and number of x for discretisation.
+ts = 0.01
+Tmax = 5.0
+N = Int(Tmax/ts) - 1
+
+#LHS of ode
+function f(x)
+    n = length(x)
+    D = Tridiagonal([ones(Float64, n) / ts ; 0.0], [1.0; -2ones(Float64, n) / ts; 1.0], [0.0; ones(Float64, n) / ts])
+    (D * [a; x; b])[2:end-1] + sin.(x)
+end
+
+#Newtons method using ForwardDiff.jl
+function newton_method_forwarddiff(f, x0, n)
+    x = x0
+    for i = 1:n
+        ∇f = ForwardDiff.jacobian(f, x)
+        x = x - ∇f \ f(x)
+    end
+    x
+end
+
+function newton_method_dualvector(f, x0, n)
+    #TODO: Implement using DualVectors.jl
+end
+
+#Initial guess
+x0 = zeros(Float64, N)
+
+#Solve and plot both solution and LHS ('deviation' from system)
+sol = newton_method_forwarddiff(f, x0, 100)
+plot(0:ts:Tmax, [a; sol; b])
+plot!(0:ts:Tmax, [0; f(sol); 0])
+
