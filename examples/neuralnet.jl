75a95a80d085685f09b7ab76365e7cb86d19abbd
maximvassiliev <vassiliev.max@gmail.com>
2024-08-07 12:29:08 +0100

    add sparse implementation and benchmark

--- examples/neuralnet.jl
+++ examples/neuralnet.jl
@@ -1,5 +1,5 @@
 import Lux: relu
-using DualArrays, LinearAlgebra, Plots
+using DualArrays, LinearAlgebra, Plots, FillArrays, Test
 
 #GOAL: Learn exp() using simple one-layer relu neural network.
 
@@ -17,14 +17,28 @@ function model_loss(w)
      sum((model(w[1:N], w[(N+1):end], data) - exp.(data)) ^ 2)
 end
 
-function gradient_descent(n, lr = 0.01)
+function gradient_descent_sparse(n, lr = 0.01)
     weights = ones(Float64, 2 * N)
     for i = 1:n
-        dw = DualVector(weights, Matrix(I, 2 * N, 2 * N))
+        dw = DualVector(weights, Eye{Float64}(2*N))
         grads = model_loss(dw).partials
         weights -= lr * grads
     end
     model(weights[1:N], weights[(N + 1):end], data)
 end
 
-plot(d:d:N*d, gradient_descent(100))
+function gradient_descent_dense(n, lr = 0.01)
+    weights = ones(Float64, 2 * N)
+    for i = 1:n
+        dw = DualVector(weights, Matrix(I, 2*N, 2*N))
+        grads = model_loss(dw).partials
+        weights -= lr * grads
+    end
+    model(weights[1:N], weights[(N + 1):end], data)
+end
+
+@time densesol = gradient_descent_dense(100)
+@time sparsesol = gradient_descent_sparse(100)
+@test densesol == sparsesol
+
+
